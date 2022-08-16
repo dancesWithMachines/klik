639a092d096c0c4af4636d2e1a160ecf2762acf3
Mateusz Kusiak (Timax) <timax2@wp.pl>
2022-08-16 19:08:50 +0200

    Add button support
    Add easy button support.
    
    Signed-off-by: Mateusz Kusiak (Timax) <timax2@wp.pl>

---
+++ button.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+/**
+ * @brief Configure a button pin.
+ *
+ * @param buttonPin button pin.
+ */
+void buttonSet(uint8_t buttonPin)
+{
+    gpio_init(buttonPin);
+    gpio_set_dir(buttonPin, GPIO_IN);
+    gpio_pull_down(buttonPin);
+}
+
+/**
+ * @brief check if button is pressed.
+ *
+ * @param buttonPin button pin.
+ * @return true - when button pressed, else false.
+ */
+bool buttonReadState(uint8_t buttonPin)
+{
+    return !gpio_get(buttonPin);
+}
---
+++ button.h
@@ -0,0 +1,7 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+void buttonSet(uint8_t buttonPin);
+bool buttonReadState(uint8_t buttonPin);
+
+#endif
